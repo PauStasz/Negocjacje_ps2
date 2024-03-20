434932ca24d015a60c554cb56347c1a48a036da4
PauStasz <paulinastasz02@gmail.com>
2024-03-20 17:19:46 +0100

    7

--- Models/Graph.py
+++ Models/Graph.py
@@ -9,8 +9,12 @@ class Graph:
         print("Tworzenie grafu...")
         self.create()
         print("Graf stworzony...")
-        print("Szukanie rozwiazania...")
-        self.findRoutesWithProfit()
+        print("Szukanie rozwiazania po raz 1...")
+        self.findRoutesWithProfit(self.__edges)
+        print("Etap 2:")
+        self.findRoutesWithProfitByTheSameWeight()
+        self.findRoutesWithProfit(self.__result)
+        print("Szukanie rozwiazania po raz 2...")
         print("Wyniki:")
         self.bestRoutes()
 
@@ -29,36 +33,36 @@ class Graph:
                                     print("Stworzona ścieżka: " + str(node.value) + " -> " + str(delivery.value) + " -> " + str(price.value) )
                                     sum = 0
                                     if(node.value == 40):
-                                        sum =  sum + 3
+                                        sum =  sum + 100
                                     elif(node.value == 42):
-                                        sum =  sum + 2
+                                        sum =  sum + 50
                                     elif(node.value == 45):
-                                        sum =  sum + 1.5
+                                        sum =  sum + 25
                                     elif(node.value == 50):
-                                        sum =  sum + 1
+                                        sum =  sum + 10
 
                                     if(delivery.value == "0-2"):
-                                        sum =  sum + 3
+                                        sum =  sum + 100
                                     elif(delivery.value == "2-7"):
-                                        sum =  sum + 2
+                                        sum =  sum + 50
                                     elif(delivery.value == "7-14"):
-                                        sum =  sum + 1
+                                        sum =  sum + 10
 
                                     
                                     if(price.value == "7-30"):
-                                        sum =  sum + 3
+                                        sum =  sum + 100
                                     elif(price.value == "0-3"):
-                                        sum =  sum + 1
+                                        sum =  sum + 10
                                     elif(price.value == "3-7"):
-                                        sum =  sum + 2
+                                        sum =  sum + 50
 
 
                                     self.__weights.append(sum) # suma ścieżki, czyli 1 krawedzi -> cena -> dostawa -> zapłata
                                     print()
 
-    def findRoutesWithProfit(self):
+    def findRoutesWithProfit(self, edges):
         
-        merged = zip(self.__weights, self.__edges)
+        merged = zip(self.__weights, edges)
 
         afterSort = sorted(merged, key=lambda x: x[0])
 
@@ -66,8 +70,48 @@ class Graph:
 
         self.__weights = sorted(self.__weights)
 
-    def bestRoutes(self):
+    def findRoutesWithProfitByTheSameWeight(self):
+         weights_to_update = [0 for _ in range(len(self.__weights))]
+         for i in range(len(self.__result)):
+            if(i + 1 < len(self.__result)):
+                weightRoute = self.__weights[i]
+                weightNextRoute = self.__weights[i + 1]
 
+                if(i > 0):
+                    weightPreviousRoute = self.__weights[i - 1]
+                else:
+                    weightPreviousRoute = 0
+
+                if(weightRoute == weightNextRoute or weightPreviousRoute == weightRoute):
+                    route = self.__result[i]
+                    if(route[0].value == 40):
+                        weights_to_update[i] =  weights_to_update[i] + 3
+                    elif(route[0] == 42):
+                        weights_to_update[i] =  weights_to_update[i] + 2
+                    elif(route[0] == 45):
+                        weights_to_update[i] =  weights_to_update[i] + 1.5
+                    elif(route[0] == 50):
+                        weights_to_update[i] =  weights_to_update[i] + 1
+
+                    if(route[1].value == "0-2"):
+                        weights_to_update[i] =  weights_to_update[i] + 3
+                    elif(route[1].value == "2-7"):
+                        weights_to_update[i] =  weights_to_update[i] + 2
+                    elif(route[1].value == "7-14"):
+                        weights_to_update[i] =  weights_to_update[i] + 1
+
+                                    
+                    if(route[2].value == "7-30"):
+                        weights_to_update[i] =  weights_to_update[i] + 3
+                    elif(route[2].value == "0-3"):
+                        weights_to_update[i] =  weights_to_update[i] + 1
+                    elif(route[2].value == "3-7"):
+                        weights_to_update[i] =  weights_to_update[i] + 2
+         
+         self.__weights = [(self.__weights[i] + weights_to_update[i]) for i in range(len(self.__weights))]
+             
+                                    
+    def bestRoutes(self):
         for i in range(36):
             route = self.__result[i]
             if isinstance(route, list):
--- program.py
+++ program.py
@@ -7,11 +7,11 @@ import matplotlib.pyplot as plt
 
 
 if __name__ == '__main__':
-    # wagi:    3                  2                    1.5           1
+    # wagi:    100/3                  50/2                    25/1.5           10/1
     nodes = [Node(40, "CENA"), Node(42, "CENA"), Node(45, "CENA"), Node(50, "CENA"),
-    #wagi:               3                    2                       1
+    #wagi:               100/3                    50/2                       10/1
              Node("0-2", "DOSTAWA"), Node("2-7", "DOSTAWA"), Node("7-14", "DOSTAWA"),
-    #wagi:          1                       2                          3
+    #wagi:          10/1                       50/2                          100/3
              Node("0-3", "ZAPŁATA"), Node("3-7", "ZAPŁATA"), Node("7-30", "ZAPŁATA")]
 
     graph = Graph(nodes)
